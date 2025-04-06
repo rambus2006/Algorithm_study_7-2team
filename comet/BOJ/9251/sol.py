57c461138a59ab0fb8687b465b504c5e5bb056b0
comet_im <hslim001@naver.com>
2025-04-06 19:20:28 +0900

    [solved] : BOJ/9251, 1799, 2293, 3952, 17281

--- comet/BOJ/9251/sol.py
+++ comet/BOJ/9251/sol.py
@@ -1,16 +1,25 @@
 import sys
 sys.stdin = open("input.txt", "r")
 #############################################
+## 시간복잡도 : 188ms
+## 공간복잡도 : 56604kb
 def find():
-    first = input().strip()
-    second = input().strip()
+    first = '.' + input().strip()
+    second = '.' + input().strip()
     if len(first) < len(second):
         first, second = second, first
-    dp = [0] * len(first)
-    cursor = 0
+    dp = [[0] * (len(second)) for _ in range(len(first))]
 
-    for i in range(len(first)):
-        for j in range(cursor, len(second)):
+    for i in range(1, len(first)):
+        temp = dp[i - 1][0]
+        for j in range(1, len(second)):
+            if first[i] == second[j]:
+                dp[i][j] = temp + 1
+
+            else:
+                dp[i][j] = dp[i - 1][j]
+            temp = max(temp, dp[i - 1][j])
+    return max(dp[-1])
 
 
 
