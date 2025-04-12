6786982fa8a3d73346a12827a727d0e594ab6a8b
bestplus <minals0520@naver.com>
2025-04-12 17:19:20 +0900

    [Refactor] : [SWEA] 22399 쉬운 당근 포장 - 코드 최적화(개선)

--- Minwoo/SWEA/22399/sol.py
+++ Minwoo/SWEA/22399/sol.py
@@ -1,39 +1,43 @@
-def is_valid(s, m, b):
-    global result
-    if not s or not m or not b:
-        return -1
-    if s[-1] == m[0] or m[-1] == b[0]:
-        return -1
-    temp = [len(s), len(m), len(b)]
-    value = max(temp) - min(temp)
-    if value > result:
-        return -1
-    return value
+# def is_valid(s, m, b):
+#     global result
+#     if not s or not m or not b:
+#         return -1
+#     if s[-1] == m[0] or m[-1] == b[0]:
+#         return -1
+#     temp = [len(s), len(m), len(b)]
+#     value = max(temp) - min(temp)
+#     if value > result:
+#         return -1
+#     return value
 
 
-def div_carrot():
-    global carrot, result
-    for i in range(N):
-        for j in range(N):
-            eds = i
-            stm, edm = eds, i + j
-            stb = edm
-            if 0 == eds or stm == edm or stb == N: continue
-            s, m, b = carrot[:eds], carrot[stm:edm], carrot[stb:]
-            value = is_valid(s, m, b)
-            if value != -1:
-                result = min(value, result)
-
+# def div_carrot():
+#     global carrot, result
+#     for i in range(N):
+#         for j in range(N):
+#             eds = i
+#             stm, edm = eds, i + j
+#             stb = edm
+#             if 0 == eds or stm == edm or stb == N: continue
+#             s, m, b = carrot[:eds], carrot[stm:edm], carrot[stb:]
+#             value = is_valid(s, m, b)
+#             if value != -1:
+#                 result = min(value, result)
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     carrot = sorted(list(map(int, input().split())))
+    carrot_box = [0] * 31
+    for i in carrot:
+        carrot_box[i] += 1
     result = float('inf')
-    div_carrot()
-    print(f"#{tc}", result if result != float('inf') else -1)
-    # break
-    # 대, 중, 소 상자로 구분
-    # 같은 크기의 당근은 같은 상자
-    # 비어있는 상자 x
-    # 각 상자에 든 개수 차이가 최소가 되도록 포장
+    for i in range(31):
+        for j in range(i, 31):
+            small, middle, big = sum(carrot_box[:i]), sum(carrot_box[i:j]), sum(carrot_box[j:])
+            temp = [small, middle, big]
+            if 0 in temp:
+                continue
+            case = max(temp) - min(temp)
+            result = min(result, case)
+    print(f'#{tc}', result if result != float('inf') else -1)
